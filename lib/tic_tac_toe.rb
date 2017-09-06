4ed7d9dffc61b35de120414e47a74784d645154e
EOSullivanBerlin <esully666@gmail.com>
2017-09-06 14:00:19 +0200

    Vertiacl and Horizontail rules tested and implamented

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,4 +9,40 @@ class Tic_Tac_Toe
   def claim_field(x_axis, y_axis, players_token)
      @grid[x_axis][y_axis] = players_token
   end
+
+  def rules
+  return "X wins with a sick horozontal!" if all_possible_horozontail_wins_X?
+  return "X wins with a devastating vertical!" if all_possibel_vertical_wins_X?
+  end
+
+
+
+  private
+
+  def all_possible_horozontail_wins_X?
+    horozontail_win_X?(0) || horozontail_win_X?(1) || horozontail_win_X?(2)
+  end
+
+  def all_possibel_vertical_wins_X?
+    vertical_win_X?(0) || vertical_win_X?(1) || vertical_win_X?(2)
+  end
+
+  def vertical_win_X?(column)
+    return true if sum_column(column) == 3
+    false
+  end
+
+  def horozontail_win_X?(row)
+    return true if sum_row(row) == 3
+    false
+  end
+
+  def sum_row(row)
+    @grid[row].inject(0){|sum,x| sum + x }
+  end
+
+  def sum_column(column)
+    @grid[0][column] + @grid[1][column] + @grid[2][column]
+  end
+
 end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -29,6 +29,42 @@ describe Tic_Tac_Toe do
   end
 
   describe '#rules' do
-    it ""
+    it "X wins when there are three horozontail Xs in the first row" do
+      game = Tic_Tac_Toe.new
+      horozontail_win(0, 1, game)
+      expect(game.rules).to eq("X wins with a sick horozontal!")
+    end
+
+    it "X wins when there are three horozontail Xs in the middle row" do
+      game = Tic_Tac_Toe.new
+      horozontail_win(1, 1, game)
+      expect(game.rules).to eq("X wins with a sick horozontal!")
+    end
+
+    it "X wins when there are three horozontail Xs in the last row" do
+      game = Tic_Tac_Toe.new
+      horozontail_win(2, 1, game)
+      expect(game.rules).to eq("X wins with a sick horozontal!")
+    end
+
+    it "X wins when there are three virtical Xs in the first column" do
+      game = Tic_Tac_Toe.new
+      vertical_win(0, 1, game)
+      p game.grid
+      expect(game.rules).to eq("X wins with a devastating vertical!")
+    end
+  end
+
+  def vertical_win(column, player_token, game)
+    game.claim_field(0, column, player_token)
+    game.claim_field(1, column, player_token)
+    game.claim_field(2, column, player_token)
+  end
+
+
+  def horozontail_win(row, player_token, game)
+    game.claim_field(row, 0, player_token)
+    game.claim_field(row, 1, player_token)
+    game.claim_field(row, 2, player_token)
   end
 end
